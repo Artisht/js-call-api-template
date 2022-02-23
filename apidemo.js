14e3ad0ec3a4944d992e3846f323830c4d15512d
Artin <artin.shakibi@skola.taby.se>
2022-02-23 14:39:56 +0100

    art

--- apidemo.js
+++ apidemo.js
@@ -1,24 +1,24 @@
-let searchText = document.getElementById("txtSearch")
+let searchText = document.getElementById("txtSearch");
 
 searchText.onkeydown = async function (event) {
-  console.log(event.key)
+  console.log(event.key);
   if (event.key === "Enter") {
-    event.preventDefault()
+    event.preventDefault();
 
     // TODO: skriv kod som hämtar värdet i sökfältet (searchText) och lägger det i
     // variabeln searchTerm
-    let searchTerm = ""
+    let searchTerm = `${searchText.value}`;
 
     // Det här anropas funktionen för att hämta info från ett API.
     // Vi väntar på svaret med await
-    let results = await search(searchTerm)
+    let results = await search(searchTerm);
 
     // Här anropas funktionen som ansvarar för att "rendera" (alltså rita ut) resultatet
-    renderResults(results)
+    renderResults(results);
 
     // Skriv kod för att tömma sökfältet igen
   }
-}
+};
 
 // Detta är en asynkron funktion som anropar ett API och returnerar svaret som ett JSON-objekt.
 async function search(searchString) {
@@ -30,37 +30,47 @@ async function search(searchString) {
     */
 
   //Här bygger vi upp den URL som vi ska använda i vårat anrop till APIet.
-  let apiKey = "" //TODO: Lägg in API-nyckeln från Classroom här.
-  var url = `https://api.themoviedb.org/3/search/movie?query=${searchString}&api_key=${apiKey}`
-  console.log("Den URL vi kommer anropa: ", url)
+  let apiKey = "1a08c634ec1bc9d64558c15c3e88cdbf"; //TODO: Lägg in API-nyckeln från Classroom här. KLAR!
+  var url = `https://api.themoviedb.org/3/search/movie?query=${searchString}&api_key=${apiKey}`;
+  console.log("Den URL vi kommer anropa: ", url);
 
   //Här används URLen för att göra anrop med den inbyggda funktionen fetch()
-  let response = await fetch(url)
+  let response = await fetch(url);
 
   // Använd console.log() för att skriva ut resultatet till konsollen och titta på det.
   // Det är ofta bra att titta på hur resultatet ser ut för att få en förståelse för
   // hur man kan skriva koden för att använda resultatet.
-  console.log(response)
+  console.log(response);
 
   // Detta gör om resultatet från APIet till ett JSON-objekt.
-  let json = await response.json()
-  return json
+  let json = await response.json();
+  return json;
 }
 
 /*
   Den här funktionen går igenom sökresultatet som är parametern "results"
   och skriver ut det i en lista i DOMen.
 */
+
 function renderResults(res) {
-  let resultDiv = document.getElementById("searchresults") //Hämtar ut diven med id="searchresults" för att lägga in resultatet där
+  let resultDiv = document.getElementById("searchresults"); //Hämtar ut diven med id="searchresults" för att lägga in resultatet där
   //Använd console.log för att se ur objektet res ser ut.
-  console.log(res)
+  console.log(res);
 
   // TODO: Hämta ut attributet av variablen res som innehåller listan med resultat
   // och tilldela variablen allObjects det värdet.
-  let allObjects = []
-  allObjects.forEach((object) => {
+  let allObjects = res.results;
+  allObjects.forEach((mov) => {
+    resultDiv.innerHTML += `
+    <div class="movie">
+      <div class="movie-title">${mov.title}</div>
+      <img class="movie-poster" src="https://image.tmdb.org/t/p/w154${mov.poster_path}">
+      <div class="movie-text">${mov.release_date}</div>
+      <div class="movie-text">${mov.overview}</div>
+    </div>
+    
+    `;
     // TODO: lägg in en div i resultDiv för varje objekt
     // du kan använda t.ex. resultDiv.insertAdjacentHTML("beforeend", "en sträng med html")
-  })
+  });
 }
